bbc44764689df14dca5cd86e37c82d9818b4bb43
fibiaan <fibianmejia@hotmail.com>
2022-06-23 10:14:26 -0500

    auto updater included

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, dialog } = require('electron');
+const { autoUpdater } = require('electron-updater');
+const isDev = require("electron-is-dev");
 
 const createWindow = () => {
     const win = new BrowserWindow({
@@ -7,7 +9,39 @@ const createWindow = () => {
     })
 
     win.loadFile('index.html')
+
+    if(!isDev){
+        autoUpdater.checkForUpdates();
+    }
+    
 }
 app.whenReady().then(() => {
     createWindow()
+})
+
+autoUpdater.on("update-available", (_event, releasesNotes, releaseName) =>{
+    const dialogOpts = {
+        type: "info",
+        buttons: ['Ok'],
+        title: "Application Update",
+        message: "Notas de versión",
+        detail: "A new version is being downloaded."
+    }
+
+    dialog.showMessageBox(dialogOpts, (response) => {});
+})
+
+autoUpdater.on("update-downloaded", (_event, releasesNotes, releaseName) => {
+    const dialogOpts = {
+        type: "info",
+        buttons: ['Restart', 'Later'],
+        title: "Application Update",
+        message: "Notas de versión",
+        detail: "Restart the application."
+    }
+    
+    dialog.showMessageBox(dialogOpts).then((returnValue) => {
+        if(returnValue.response === 0) autoUpdater.quitAndInstall();
+    });
+
 })
