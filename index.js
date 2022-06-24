78744c3abcaf6ff9bac1c8818b5ae32bc1762ba2
Cesar Peña <cesarphernandez09@gmail.com>
2022-06-24 11:20:30 -0500

    Adjustments

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const { app, BrowserWindow, dialog } = require('electron');
+const { app, BrowserWindow, Menu, dialog, Tray, nativeImage, nativeTheme } = require('electron');
 const { autoUpdater } = require('electron-updater');
 const isDev = require("electron-is-dev");
-
+const path = require("path");
+let tray = null;
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 800,
@@ -18,9 +19,83 @@ const createWindow = () => {
     }
     
 }
-app.whenReady().then(() => {
-    createWindow()
+app.whenReady().then(async () => {
+    try {
+        createWindow()
+        console.log(app.getAppPath())
+        tray = new Tray(nativeImage.createFromPath(await validateThemeIcon()).resize({ width: 16 }));
+        tray.setToolTip("Mocukp Baker App");
+        tray.setContextMenu(Menu.buildFromTemplate(menuTrayTemplate));
+    } catch (error) {
+    }
 })
+const validateThemeIcon = async () => {
+    if (nativeTheme.shouldUseDarkColors) {
+      return path.join(__dirname, `assets/baker-tray-icon-light.png`);
+    } else {
+      return path.join(__dirname, `assets/baker-tray-icon.png`);
+    }
+}
+let menuTrayTemplate = [
+    {
+      label: "About Mockup Baker..",
+      icon: nativeImage
+        .createFromPath(__dirname + "/assets/baker-tray-icon.png")
+        .resize({ width: 16 }),
+      click: function () {},
+    },
+    {
+        label: "Probe update..",
+        icon: nativeImage
+          .createFromPath(__dirname + "/assets/baker-tray-icon.png")
+          .resize({ width: 16 }),
+        click: function () {},
+      },
+    {
+      type: "separator",
+    },
+    {
+      label: "Install Plugin",
+      id: "plugin",
+      click: function () {
+        if (status.plugin == "toInstall") {
+          installPlugin();
+        } else {
+          uninstallPlugin();
+        }
+      },
+    },
+    {
+      label: "Start Server",
+      id: "server",
+      click: async function () {
+        changeServer(!status.server);
+      },
+    },
+    {
+      type: "separator",
+    },
+    {
+      label: "Help",
+      id: "help",
+      click: function () {
+        shell.openExternal("https://originalmockups.com");
+      },
+    },
+    {
+      label: "Preferences",
+      id: "preferences",
+      accelerator: process.platform == "darwin" ? "Command+," : "Ctrl+Q",
+      click: function () {},
+    },
+    {
+      label: "Quit",
+      accelerator: process.platform == "darwin" ? "Command+Q" : "Ctrl+Q",
+      click: function () {
+        app.quit();
+      },
+    },
+  ];
 
 autoUpdater.on("update-available", (_event, releasesNotes, releaseName) =>{
     const dialogOpts = {
